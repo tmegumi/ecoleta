5a42fd3b5fd7884c3684d1aded7a8b0aa7889dd0
Thais Megumi <tha.megumi@gmail.com>
2020-06-06 12:00:49 -0300

    Create Ecoleta application

---
+++ server/src/routes.ts
@@ -0,0 +1,40 @@
+import express from 'express';
+import { celebrate, Joi } from 'celebrate';
+
+import multer from 'multer';
+import multerConfig from './config/multer';
+
+import ItemsController from './controllers/ItemsController';
+import PointsController from './controllers/PointsController';
+
+const routes = express.Router();
+const upload = multer(multerConfig);
+
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
+
+routes.get('/items', itemsController.index);
+routes.get('/points', pointsController.index);
+routes.get('/points/:id', pointsController.show);
+
+routes.post(
+  '/points', 
+  upload.single('image'),
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().required(),
+      email: Joi.string().required().email(),
+      whataspp: Joi.number().required(),
+      latitude: Joi.string().required,
+      longitude: Joi.string().required,
+      city: Joi.string().required(),
+      uf: Joi.string().required().max(2),
+      items: Joi.string().required()
+    })
+  }, {
+    abortEarly: false
+  }),
+  pointsController.create
+);
+
+export default routes;
